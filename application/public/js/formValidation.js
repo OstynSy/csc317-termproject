c48d2fc5cf03579c0effe7dbf98cf1b4ab533956
Ostyn Sy <osy304@insite.4cd.edu>
2020-12-09 01:14:48 -0800

    incorporated other assignments as well as combined files

---
+++ application/public/js/formValidation.js
@@ -0,0 +1,96 @@
+function beginChar(text) {   
+    return (/[a-zA-Z]/).test(text.charAt(0));
+}
+
+function threeAlpha(text) {
+    var count = 0;
+    var i;
+    for (i = 0; i < text.length; i++) {
+        if ((/[a-zA-Z]/).test(text.charAt(i)) == true) {
+            count++;
+        }
+    }
+    if (count >= 3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function charLength(text) {
+    if (text.length >= 8) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function containsUpper(text) {
+    return (/[A-Z]/).test(text);
+}
+
+function containsNumber(text) {
+    return (/\d/).test(text);
+}
+
+function containsSpecial(text) {
+    return (/[/*-+!@#$^&*]/).test(text);
+} 
+
+
+function passwordMatch(pass, cpass) {
+    if (pass == cpass) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+function errorMessage() {
+    var validated = true;
+    var error = document.getElementById("error");
+    var username = document.registerform.uname.value;
+    var password = document.registerform.pword.value;
+    var cpassword = document.registerform.cpword.value;
+
+    if (beginChar(username) != true) {
+        error.innerHTML = "*Username must begin with a letter";
+        validated = false;
+    }
+    else if(threeAlpha(username) != true) {
+        error.innerHTML = "*Username must contain at least 3 letters";
+        validated = false;
+    }
+    else if (charLength(username) != true) {
+        error.innerHTML = "*Username must contain at least 8 characters";
+        validated = false;
+    }
+    else if (containsUpper(username) != true) {
+        error.innerHTML = "*Username must contain at least 1 Uppercase";
+        validated = false;
+    }
+    else if (containsNumber(username) != true) {
+        error.innerHTML = "*Username must contain at least 1 Number";
+        validated = false;
+    }
+    else if (containsSpecial(username) != true) {
+        error.innerHTML = "*Username must contain at least 1 Special character (/*-+!@#$^&*)";
+        validated = false;
+    }
+    else if (passwordMatch(password, cpassword) != true) {
+        error.innerHTML = "*Passwords do not match";
+        validated = false;
+    }
+
+    else {
+        error.innerHTML = "";
+    }
+
+    return validated;
+}
+
--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+    res.render('index',{title:"Ostyn APP"});
+});
+
+//localhost:3000/login
+router.get('/login', (req, res, next) => {
+    res.render("login", { title: "Log In" });
+});
+
+//localhost:3000/registration
+router.get('/registration', (req, res, next) => {
+    res.render("registration", {title: "Register" });
+});
+
+//localhost:3000/postimage
+router.get('/postimage', (req, res, next) => {
+    res.render("postimage", {title: "Create Post" });
+});
+
+//localhost:3000/logout
+router.get('/logout', function (req, res, next) {
+    res.send('hello logout.html')
 });
 
 module.exports = router;
